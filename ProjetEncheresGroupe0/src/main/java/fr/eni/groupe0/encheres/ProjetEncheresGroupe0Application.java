82aa5e2e713b8afc161b7c3a3853136a69c5e69b
ssultan <ssultan@12SE22-HXB14G2.ad.campus-eni.fr>
2023-06-26 10:10:01 +0200

    initialisation du projet

---
+++ ProjetEncheresGroupe0/src/main/java/fr/eni/groupe0/encheres/ProjetEncheresGroupe0Application.java
@@ -0,0 +1,13 @@
+package fr.eni.groupe0.encheres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetEncheresGroupe0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetEncheresGroupe0Application.class, args);
+	}
+
+}
